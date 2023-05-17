621d647992c8baf9277d844dc8231f81a98be678
woshixiaobai1hao <133489684+woshixiaobai1hao@users.noreply.github.com>
2023-05-17 18:20:28 +0800

    Add files via upload

---
+++ test/test/test.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+////int main()
+////{
+////	int i = 0;
+////	
+////	for (i = 0; i <= 100; i++)
+////	{
+////		printf("%d ", i);
+////	}
+//	for (i = 0; i <= 100; i++)
+//	{
+//		printf("%d ",10-i);
+//	}
+//	
+//	return 0;
+//}
+//i/*nt main()
+//{
+//	int i = 0;
+//	int arr[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//	for (i = 0; i <= 12; i++)
+//	{
+//		printf("hehe\n");
+//		arr[i]=0;
+//	}
+//	system("pause");
+//	return 0;
+//}*/
+int main()
+{
+	int i = 0;
+	int a = 10;
+	printf("hh");
+	return 0;
+}
+
+
